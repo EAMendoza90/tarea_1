ac916f3461e5608c69a018f05eec322b6872a34c
ALFRED-VJ-DEV <alfred.vj.dev@gmail.com>
2025-07-27 11:13:48 -0600

    ref: refactor retroceder and avanzar

--- ejercicioApi.js
+++ ejercicioApi.js
@@ -20,7 +20,7 @@ async function cargarApi() {
     console.log(`[DEBBUG] F-CARGAR API/data: `, data);
     paginator.innerText = "pagina: 1";
     const containerNames = document.getElementById('containerNames');
-   changesContainer(containerNames, data)
+    changesContainer(containerNames, data)
     /* data.results.forEach((element, index) => {
         const p = document.createElement('p');
         p.innerText = `${index + 1} - ${element.name}`;
@@ -28,44 +28,25 @@ async function cargarApi() {
     });*/
 }
 
-
 async function retroceder() {
     if (!endpointAnterior) {
         return;
     }
-    const response1 = await fetch(endpointAnterior);
     console.log("[DEBBUG] endpointAnterior: ", endpointAnterior); //null
+    const response1 = await fetch(endpointAnterior);
     const data1 = await response1.json();
-    console.log({DATA: data1})
-    //TODO: resolver el error del paginador, refactorizar, corregir la variable indiceActual (ya no utilizarla)
     const paginator = document.getElementById("paginator")
-    console.log({ paginator, paginatoriT: paginator.innerText })
     endpointSiguiente = data1.info.next
     endpointAnterior = data1.info.prev
     const containerNames = document.getElementById('containerNames');
-    if (data1.info.next) {
-            if (data1.info.next[data1.info.next.length - 2] == '=') {
-            paginator.innerText = "pagina: " + (Number(data1.info.next[data1.info.next.length - 1]) - 1)
-        } else {
-            paginator.innerText = "pagina: " + (Number(data1.info.next[data1.info.next.length - 2] + data1.info.next[data1.info.next.length - 1]) - 1)
-        }
-        changesContainer(containerNames, data1);
-        console.log(`endpoint consumido: `, data1.info.next)
-        console.log(`dataC: `,  (Number(data1.info.next[data1.info.next.length - 1]) - 1) )
-
-    } else if (data1.info.prev) {
-        paginator.innerText = "pagina: " + (Number(data1.info.prev[data1.info.prev.length - 1]) + 1)
-        console.log(`dataD: `,  (Number(data1.info.prev[data1.info.prev.length - 1]) - 1))
+    if (data1.info.next[data1.info.next.length - 2] == '=') {
+        paginator.innerText = "pagina: " + (Number(data1.info.next[data1.info.next.length - 1]) - 1)
+    } else {
+        paginator.innerText = "pagina: " + (Number(data1.info.next[data1.info.next.length - 2] + data1.info.next[data1.info.next.length - 1]) - 1)
     }
-    console.log("[debugg] f-retroceder: ", endpointAnterior)
+    changesContainer(containerNames, data1);
 }
-/*
-Ya tenemos el consumo de la api
--consumir el endpoint de la varable endpointSiguiente
--cambiar el valor de pagina
--cambiar la lista por la lista actualizada
--actualizar el endpointSiguiente y el endpointAnterior
-*/
+
 async function avanzar() {
     if (!endpointSiguiente) return
     const response2 = await fetch(endpointSiguiente);
@@ -73,42 +54,35 @@ async function avanzar() {
     const paginator = document.getElementById("paginator")
     const containerNames = document.getElementById('containerNames');
     endpointAnterior = data2.info.prev
-
-    if (data2.info.next) {
-        endpointSiguiente = data2.info.next
-        if (data2.info.next[data2.info.next.length - 2] == '=') {
-            paginator.innerText = "pagina: " + (Number(data2.info.next[data2.info.next.length - 1]) - 1)
-        } else {
-            paginator.innerText = "pagina: " + (Number(data2.info.next[data2.info.next.length - 2] + data2.info.next[data2.info.next.length - 1]) - 1)
-        }
-        changesContainer(containerNames, data2);
+    endpointSiguiente = data2.info.next
+    if (data2.info.prev[data2.info.prev.length - 2] == '=') {
+        paginator.innerText = "pagina: " + (Number(data2.info.prev[data2.info.prev.length - 1]) + 1)
     } else {
-         paginator.innerText = "pagina: " + (endpointSiguiente[endpointSiguiente.length - 2] + endpointSiguiente[endpointSiguiente.length - 1])
-        endpointSiguiente = null;
-        changesContainer(containerNames, data2);
+        paginator.innerText = "pagina: " + (Number(data2.info.prev[data2.info.prev.length - 2] + data2.info.prev[data2.info.prev.length - 1]) + 1)
     }
+    changesContainer(containerNames, data2);
 }
 
- function changesContainer(containerNames, data2) {
-        while (containerNames.firstChild) {
-            containerNames.removeChild(containerNames.firstChild);
-        }
-
-        data2.results.forEach((element) => {
-            const p = document.createElement('p');
-            p.innerText = `${element.id} - ${element.name}`;
-            containerNames.appendChild(p);
-        });
+function changesContainer(containerNames, data2) {
+    while (containerNames.firstChild) {
+        containerNames.removeChild(containerNames.firstChild);
     }
 
-     function uploadImages(containerNames, data2) {
-        while (containerNames.firstChild) {
-            containerNames.removeChild(containerNames.firstChild);
-        }
+    data2.results.forEach((element) => {
+        const p = document.createElement('p');
+        p.innerText = `${element.id} - ${element.name}`;
+        containerNames.appendChild(p);
+    });
+}
 
-        data2.results.image.forEach((element) => {
-            const imagen = document.createElement('img');
-            imagen.src= data2.results.image
-            containerNames.appendChild(imagen);
-        });
+function uploadImages(containerNames, data2) {
+    while (containerNames.firstChild) {
+        containerNames.removeChild(containerNames.firstChild);
     }
+
+    data2.results.image.forEach((element) => {
+        const imagen = document.createElement('img');
+        imagen.src = data2.results.image
+        containerNames.appendChild(imagen);
+    });
+}
